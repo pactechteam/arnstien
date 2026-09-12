9b81d11654060a7ea4e9c0c5af1e95ceeb41dccd
Zach <zach.kohl@gmail.com>
2026-09-12 16:52:35 -0700

    wip

--- src/postProcessor.ts
+++ src/postProcessor.ts
@@ -1,6 +1,6 @@
 import { App, normalizePath, Notice, requestUrl, TFolder } from "obsidian"
 import { buildCodeBlock, targetFolder } from "./handlePaste"
-export const addressBase = 'http://localhost:3000/'//'https://www.paccenter.org/'
+export const addressBase = 'https://www.paccenter.org/'
 export function postProcessor(context: any) {
 
 
